9a75c35f9e3353c17684cfb592cde5f51ac53065
shenxx <shenxiangxiang@gmail.com>
2023-12-28 15:49:11 +0800

    build.type to user

--- core/sysprop.mk
+++ core/sysprop.mk
@@ -67,7 +67,7 @@ define generate-common-build-props
     echo "ro.$(1).build.fingerprint=$(BUILD_FINGERPRINT_FROM_FILE)" >> $(2);\
     echo "ro.$(1).build.id=$(BUILD_ID)" >> $(2);\
     echo "ro.$(1).build.tags=$(BUILD_VERSION_TAGS)" >> $(2);\
-    echo "ro.$(1).build.type=$(TARGET_BUILD_VARIANT)" >> $(2);\
+    echo "ro.$(1).build.type=user" >> $(2);\
     echo "ro.$(1).build.version.incremental=$(BUILD_NUMBER_FROM_FILE)" >> $(2);\
     echo "ro.$(1).build.version.release=$(PLATFORM_VERSION_LAST_STABLE)" >> $(2);\
     echo "ro.$(1).build.version.release_or_codename=$(PLATFORM_VERSION)" >> $(2);\
